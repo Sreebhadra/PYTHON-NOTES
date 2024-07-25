845fd6a6d461cf6b56ab4670db7f4231ca7ce11e
SREEBHADRA <76495709+Sreebhadra@users.noreply.github.com>
2024-07-25 14:34:27 +0530

    Add files via upload

---
+++ PYTHON_LEARNING/myfile.py
@@ -0,0 +1,6 @@
+def my_function():
+    print("To import you own package, first create a file named myfile.py in the same directory as the script or in Python's module search path.")
+    print(" The custom module named myfile likely contains at least one function called my_function().")
+    print("Then call the function named my_function() that is defined within the myfile module.")
+    
+my_function()
